737d2da0a5068ebfea10ab91001d220ddfbf06fa
DobbyK <69256884+DobbyK@users.noreply.github.com>
2020-11-10 04:59:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gaming-functions-DobbyK", # Replace with your own username
+    name="DobbyK", # Replace with your own username
     version="0.1.3",
     author="Dobby King",
     author_email="dobbyk@techie.com",
