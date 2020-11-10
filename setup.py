6df600b46a6b2d08b249ae7ac5ffce4f6b567050
DobbyK <69256884+DobbyK@users.noreply.github.com>
2020-11-10 04:43:35 -0700

    v0.1.2

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ setup(
     name="functions for games",
     version="0.1.1",
     description="All the function you need to make a game in python and pygam",
-    long_description=README,
-    long_description_content_type="text/markdown",
     url="https://github.com/DobbyK/CustomFunctions",
     author= "",
     author_email="dobbyk@techie.com",
