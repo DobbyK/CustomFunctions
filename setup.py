7c6f10235cb9970ee1d1e1e55375a9ec7ac7bfa5
DobbyK <69256884+DobbyK@users.noreply.github.com>
2020-11-10 04:55:27 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-import pathlib
-from setuptools import setup
-setup(
-    name="functions for games",
-    version="0.1.1",
-    description="All the function you need to make a game in python and pygam",
-    url="https://github.com/DobbyK/CustomFunctions",
-    author= "",
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gaming-functions-DobbyK", # Replace with your own username
+    version="0.1.3",
+    author="Dobby King",
     author_email="dobbyk@techie.com",
-    license="None",
+    description="A small package not really meant for production, learning mostly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DobbyK/Heartlife",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
